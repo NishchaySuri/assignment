7b35014d1136190c3d7b4f5118b695cdb2348cc9
Nishchay Suri <surinishchay@gmail.com>
2017-03-04 02:41:00 +0530

    Advance

--- DLA.py
+++ DLA.py
@@ -1,6 +1,9 @@
 '''
 Locus.sh Assignment
-Diffusion Limited Aggregation (DLA)
+Diffusion Limited Aggregation (DLA) 
+
+- Matrix M X M where M is an odd integer
+- Image is toroidally bound
 
 -Nishchay Suri
 155120029
@@ -11,14 +14,20 @@ import random as rnd
 
 class DLA():
 	def __init__(self,dim,num,stickiness):
-		self.M = dim						# Matrix is of the dimention : M X M (Assuming M is odd)
+		self.M = dim	            # Matrix is of the dimention : M X M (Assuming M is odd)
 		self.num = num				# num is the number of Particles
 		self.stickiness = stickiness		 
 		self.Matrix = self.Initialize_Matrix()
+		self.stuck = False      	# Turns True when a particle gets stuck
 		
 		# Location of new Particle Introduced along the border
 		self.row=0					
 		self.col=0
+
+		# Array of Adjacent indices in the Matrix
+		self.adj = np.array([[self.row-1,self.col-1], [self.row-1,self.col], [self.row-1,self.col+1],
+							 [self.row,self.col-1]  ,						 [self.row,self.col+1],
+							 [self.row+1,self.col-1], [self.row+1,self.col], [self.row+1,self.col+1]])
 
 
 
@@ -76,7 +85,77 @@ class DLA():
 
 
 
-	
+	# Gets adjacent elements from (row,col)  (Matrix is toroidally bound)
+	def get_adjacent(self,row,col):
+		self.adj = np.array([  [row-1,col-1], [row-1,col], [row-1,col+1],
+							   [row,col-1]  ,			   [row,col+1],
+							   [row+1,col-1], [row+1,col], [row+1,col+1]])
+
+		#For wrapping
+		for r in range(0,8):
+			for c in range(0,2):
+				if(self.adj[r,c] == -1):
+					self.adj[r,c] = self.M-1
+
+				elif(self.adj[r,c] == self.M):
+					self.adj[r,c] = 0
+
+		return(self.adj)
+
+
+
+
+	# Takes a random step from the element (row,col) by checking all adjacent cells and updates (self.row,self.col)
+	def random_step(self,row,col):
+		flag = False							# Flag turns True when a neighbouring cell has element 1
+		self.adj = self.get_adjacent(row,col)
+
+		# Counting number of neighbours having element 1 
+		list_elements = []   					
+		for elem in self.adj:
+			if(self.Matrix[elem[0],elem[1]] == 1):
+				flag = True
+				list_elements.append(elem)    
+		
+
+		# Removes the elements from adjacency list which has element 1
+		self.adj = np.delete(self.adj,list_elements,0) 
+
+
+		# If no neighbouring element is 1 
+		if(flag == False): 
+			self.Matrix[row,col] = 0
+			num = rnd.randint(0,7)
+			self.row , self.col = self.adj[num]
+			self.Matrix[self.row,self.col] = 1
+		
+		# If a neighbouring element is 1 and particle does not stick probabilistically
+		elif(flag == True and rnd.random() > self.stickiness and len(self.adj)!=0):
+			num = rnd.randint(0,len(self.adj)-1)
+			self.Matrix[row,col] = 0
+			self.row , self.col = self.adj[num]
+			self.Matrix[self.row,self.col] = 1
+		
+		else:
+			self.stuck = True
+			self.Matrix[row,col] = 1 
+
+		
+
+
+
+	# def Run_simulation(self):
+
+	# 	for i in range(0,self.num):
+	# 		self.stuck = False
+
+	# 		while(!self.stuck):
+	# 			self.Intro_New_Particle()
+
+
+
+
+
 
 
 
@@ -86,4 +165,4 @@ class DLA():
 
 if __name__=='__main__':
 	a = DLA(5,1,1)
-		
+	a.random_step(0,0)		
