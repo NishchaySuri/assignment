5e28846c2c028eb5638c9aabed4db188f16d49ee
Nishchay Suri <surinishchay@gmail.com>
2017-03-06 03:10:27 +0530

    Final

--- newDLA.py
+++ newDLA.py
@@ -15,10 +15,60 @@ import random as rnd
 import matplotlib.pyplot as plt
 
 class DLA():
-	def __init__(self,dim,num,stickiness):
+
+	'''
+	DLA Class : Diffusion Limited Aggregation
+
+	INPUT:
+	1st Parameter : Dimention M (odd) of  M X M Matrix
+
+					This is the input number M for M X M pixels/cells for an image/matrix.
+
+
+	2nd Parameter : Number of Particles
+
+					This is the number of particles for building the structure.
+
+
+	3rd Parameter : Stickiness from [0,1]
+
+					Stickiness is a parameter depending on the interaction of particles amoung each other.
+
+																					   ####################################################	
+	4th Parameter : Measure of distance of innter matrix from the structure (dist)  <- # Responsible for efficiency and quality of figure #
+																					   ####################################################
+					We take an inner matrix within the bigger M X M matrix. We introduce particles along it's border as it is closer to the structure. 
+					After every few iterations we calculate the farthest point of the structure and introduce this border at a (dist) distance away.
+
+					This inner matrix is torroidally bound as there is a much higher chance of particles sticking if they return closer to the structure.
+					Appropriately large the (dist) parameter better 'Random Walks' we get as the particles introduced along the border have more space 
+					to move freely.
+
+
+	#################			Intro_New_Particle = Introduces new particle along the border of inner matrix (dim_temp X dim_temp)
+	#				#								 The particle random walks inside this inner matrix and is torroidally bound inside it
+	#				#								 to achieve greater efficiency. 
+	#				#								 The size of the inner matrix is increased as the structure grows.
+	#	 #######	#
+	#	 #	   #	#
+	#	 #  1  #    #    		Intro_New_Particle_anywhere = Introduces new particle inside the inner matrix (for the sake of increasing efficiency)
+	#	 #     #	#						   
+	#	 #######	#						  
+	#				#						  
+	#				#						  
+	#				#		
+	#################		
+					 M X M	
+
+
+	'''
+
+
+	def __init__(self,dim,num,stickiness,dist):
 		self.M = dim	            # Matrix is of the dimention : M X M (Assuming M is odd)
 		self.num = num				# num is the number of Particles
-
+		self.max_dimention = dim
+		self.dist = dist
 		# Centre of the Large M X M matrix
 		self.row_centre = (dim-1)/2
 		self.col_centre = (dim-1)/2
@@ -30,6 +80,10 @@ class DLA():
 		# Location of new Particle Introduced along the border
 		self.row=0					
 		self.col=0
+
+		# Max row and Max col
+		self.max_row = (self.max_dimention-1)/2
+		self.max_col = (self.max_dimention-1)/2
 
 		# Array of Adjacent indices in the Matrix
 		self.adj = np.array([[self.row-1,self.col-1], [self.row-1,self.col], [self.row-1,self.col+1],
@@ -54,24 +108,7 @@ class DLA():
 
 
 
-	''' 
-	#################
-	#				#
-	#				#
-	#	|->			#
-	#	 #######	#
-	#	 #	   #	#
-	#	 #  1  #    #    Intro_New_Particle = Introduces new particle along the border of inner matrix (dim_temp X dim_temp)
-	#	 #     #	#						  The dimention changes changes as the simulation grows
-	#	 #######	#
-	#				#
-	#				#
-	#				#
-	#################
-					 M X M
 
-	
-	'''
 	def Intro_New_Particle(self,dim_temp): # Randomly introduces a new particle along the border of dim_temp around the bigger matrix
 		flag = True
 		self.M = dim_temp
@@ -112,15 +149,18 @@ class DLA():
 
 
 	# Randomly introduces a new particle anywhere in the matrix (To increase efficiency)
-	def Intro_New_Particle_anywhere(self):
+	def Intro_New_Particle_anywhere(self,dim):
 		flag = True
+		self.M = dim
+		r_offset = self.row_centre - (dim-1)/2
+		c_offset = self.col_centre - (dim-1)/2
 		while(flag):
 			rnd_row = rnd.randint(0,self.M-1)
 			rnd_col = rnd.randint(0,self.M-1)
-			if(self.Matrix[rnd_row,rnd_col] == 0):
-				self.Matrix[rnd_row,rnd_col] = 1
-				self.row = rnd_row
-				self.col = rnd_col
+			if(self.Matrix[r_offset + rnd_row, c_offset + rnd_col] == 0):
+				self.Matrix[r_offset+ rnd_row, c_offset + rnd_col] = 1
+				self.row =  r_offset + rnd_row
+				self.col =  c_offset + rnd_col
 				flag = False
 
 
@@ -146,7 +186,6 @@ class DLA():
 
 
 
-
 	# Takes a random step from the element (row,col) by checking all adjacent cells and updates (self.row,self.col)
 	def random_step(self,row,col,dim_temp):
 		flag = False							# Flag turns True when a neighbouring cell has element 1
@@ -156,12 +195,7 @@ class DLA():
 		for elem in self.adj:
 			if(self.Matrix[elem[0],elem[1]] == 1):
 				flag = True
-				break  
-		
-
-		# Removes the elements from adjacency list which has element 1
-		#self.adj = np.delete(self.adj,list_elements,0) 
-
+				break 
 
 		# If no neighbouring element is 1 
 		if(flag == False): 
@@ -176,66 +210,37 @@ class DLA():
 			for eme in self.adj:
 				if(self.Matrix[elem[0],elem[1]] == 0):
 					l.append([elem[0],elem[1]])
-					num = rnd.randint(0,len(l)-1)
-					self.Matrix[row,col] = 0
-					self.row , self.col = l[num]
-					self.Matrix[self.row,self.col] = 1
+			if len(l) != 0:
+				num = rnd.randint(0,len(l))
+				self.Matrix[row,col] = 0
+				self.row , self.col = l[num]
+				self.Matrix[self.row,self.col] = 1
 		
 		else:
 			self.stuck = True
 			self.Matrix[row,col] = 1 
+			if(  (row-self.row_centre)**2 + (col-self.col_centre)**2 > (self.max_row-self.row_centre)**2 + (self.max_col-self.col_centre)**2 ) :
+				self.max_row , self.max_col = row , col
 
 		
 
 
 	# Run for N particles 
 	def Run_simulation(self):
+		dim_temp = 51
 		for i in range(0,self.num):
 			if(i%100 == 0):
-			 	print i
+				print i,dim_temp
 
-			if i<=100:
-				self.Intro_New_Particle(51)
-				self.stuck = False
-
-				while(self.stuck == False):
-					self.random_step(self.row,self.col,51)
-
-			elif i>100 and i<=600:
-				self.Intro_New_Particle(101)
-				self.stuck = False
-
-				while(self.stuck == False):
-					self.random_step(self.row,self.col,101)
-
-			elif i>600 and i<=2500:
-				self.Intro_New_Particle(201)
-				self.stuck = False
-
-				while(self.stuck == False):
-					self.random_step(self.row,self.col,201)
-
-			elif i>2500 and i<=4500:
-				self.Intro_New_Particle(301)
-				self.stuck = False
-
-				while(self.stuck == False):
-					self.random_step(self.row,self.col,301)
-
-			elif i>4500 and i<=9000:
-				self.Intro_New_Particle(401)
-				self.stuck = False
-
-				while(self.stuck == False):
-					self.random_step(self.row,self.col,401)
-
-			elif i>9000:
-				self.Intro_New_Particle(501)
-				self.stuck = False
-
-				while(self.stuck == False):
-					self.random_step(self.row,self.col,501)
-
+				if dim_temp < d.max_dimention:
+					dim_temp = max(dim_temp, self.dist + 2* np.abs((max(self.max_row,self.max_col) - self.row_centre)))
+				else:
+					dim_temp = d.max_dimention
+			
+			self.Intro_New_Particle_anywhere(dim_temp)
+			self.stuck = False
+			while(self.stuck == False):
+				self.random_step(self.row,self.col,dim_temp)
 
 
 
@@ -246,16 +251,19 @@ INPUT:
 1st Parameter : Dimention M (odd) of  M X M Matrix
 2nd Parameter : Number of Particles
 3rd Parameter : Stickiness from [0,1]
+4th Parameter : Measure of distance of innter matrix from the structure
 '''
 
 if __name__=='__main__':
-	d = DLA(501,10000,1)
+	d = DLA(501,20000,1,51)
+
 	start = timer()
 	d.Run_simulation()
 	end = timer()
-	np.savetxt('matrix.txt',d.Matrix,fmt='%d')
 
+	np.savetxt('matrix.txt',d.Matrix,fmt='%d')
 	print("Time elapsed {}".format(end-start))
+	d.plot()
 
 
 
